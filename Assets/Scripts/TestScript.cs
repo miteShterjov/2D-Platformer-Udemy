1577ed550d1782b5791b6ac9caf93a084219d5d5
mite.shterjov <mite.shterjov@gmail.com>
2025-10-23 19:03:42 +0200

    Add initial scene and script setup for 2D platformer project

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("TestScript has started.");
+        Debug.Log("This is a Hello World type of message.");   
+    }
+
+}
